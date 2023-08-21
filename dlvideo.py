8172245fbc13979be875b10a05a7647af75e7cda
Jeremy - Yacla <120169275+jeremyYacla@users.noreply.github.com>
2023-08-21 15:40:02 +0200

    Update dlvideo.py

--- dlvideo.py
+++ dlvideo.py
@@ -1,68 +1,69 @@
-import requests
 import os
+from moviepy.editor import VideoFileClip, concatenate_videoclips, ColorClip
 
-CLIENT_ID = 'be9b625a673e79c'
-HEADERS = {
-    'Authorization': f'Client-ID {CLIENT_ID}'
-}
+def is_valid_video(video_path):
+    """Verify if the video can be loaded and played without issues."""
+    try:
+        with VideoFileClip(video_path) as clip:
+            frame = clip.get_frame(0)
+            if clip.audio is None:
+                print(f"{video_path} has no audio. Skipping...")
+                return False
+            return True
+    except:
+        print(f"Error validating {video_path}. Skipping...")
+        return False
 
-IMGUR_API_URL = 'https://api.imgur.com/3/gallery/top/viral/0?showViral=true&mature=true&album_previews=true'
-DOWNLOAD_FOLDER = 'videozap'  # Changement du chemin
+def process_video_clip(video_path):
+    """Resizes and centers the video clip according to the final format."""
+    clip = VideoFileClip(video_path)
+    if clip.size[0] > clip.size[1]:  # Landscape video
+        clip = clip.resize(height=1080)  # Resize it to 1080 in height
+        # Now, we'll center it in a 1080x1920 frame
+        background = ColorClip((1080, 1920), color=(0, 0, 0)).set_duration(clip.duration)
+        clip = CompositeVideoClip([background, clip.set_position("center")])
+    else:
+        clip = clip.resize(width=1080)  # Portrait video
+    return clip
 
-def download_file(url, filename):
-    print(f"Downloading from {url} to {filename}")
-    with requests.get(url, stream=True) as response:
-        response.raise_for_status()
-        with open(filename, 'wb') as out_file:
-            for chunk in response.iter_content(chunk_size=8192):
-                out_file.write(chunk)
-
-def get_video_from_album(album_id):
-    url = f"https://api.imgur.com/3/album/{album_id}"
-    response = requests.get(url, headers=HEADERS)
-    data = response.json()
+def create_zap(directory_path, transition_video_path, output_path="output.mp4"):
+    print("Starting create_zap function...")
     
-    if not data['success']:
-        print(f'Failed to retrieve data for album {album_id}.')
-        return None
-    
-    album_data = data['data']
-    videos = [image for image in album_data['images'] if 'type' in image and image['type'].startswith('video/')]
-    
-    if videos:
-        return videos[0]  # Retourne la première vidéo trouvée dans l'album
-    return None
+    video_files = [f for f in os.listdir(directory_path) if f.endswith('.mp4') and f != os.path.basename(transition_video_path)]
+    video_files = sorted(video_files)
+    print(f"Found {len(video_files)} video files in the directory.")
 
-def get_top_50_imgur_videos():
-    response = requests.get(IMGUR_API_URL, headers=HEADERS)
-    data = response.json()
+    clips = []
 
-    if not data['success']:
-        print('Failed to retrieve data from Imgur.')
-        return []
+    transition_clip = VideoFileClip(transition_video_path).resize(width=1080)  # Ensure the transition is also resized to 1080x1920
+    print("Loaded and resized transition video.")
 
-    videos = []
+    for video_file in video_files:
+        video_path = os.path.join(directory_path, video_file)
+        if not is_valid_video(video_path):
+            continue
 
-    for item in data['data']:
-        # Si c'est une vidéo directe
-        if 'type' in item and item['type'].startswith('video/'):
-            videos.append(item)
-        # Si c'est un album qui contient une vidéo
-        elif item.get('is_album') and 'images' in item:
-            for image in item['images']:
-                if image['type'].startswith('video/'):
-                    videos.append(image)
-    
-    print(f"Found {len(videos)} videos.")
-    return videos
+        try:
+            video_clip = process_video_clip(video_path)
+            if video_clip:
+                clips.append(video_clip)
+                clips.append(transition_clip)
+        except Exception as e:
+            print(f"Error processing video {video_file}: {e}")
 
-def main():
-    os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
-    videos = get_top_50_imgur_videos()
-
-    for video in videos:
-        filename = os.path.join(DOWNLOAD_FOLDER, video['id'] + '.mp4')  # Assuming all videos are mp4 format
-        download_file(video['link'], filename)
+    try:
+        if clips:
+            print(f"Attempting to concatenate {len(clips)//2} videos and {len(clips)//2} transitions.")
+            final_clip = concatenate_videoclips(clips[:-1], method="compose")  # We exclude the last transition
+            final_clip.write_videofile(output_path, codec="libx264", audio_codec='aac')
+            print("Final video written successfully.")
+        else:
+            print("No valid clips found to concatenate.")
+    except Exception as e:
+        print(f"Error during final video creation: {e}")
 
 if __name__ == "__main__":
-    main()
+    directory = "videozap"
+    transition = "transition.mp4"
+    
+    create_zap(directory, transition)
